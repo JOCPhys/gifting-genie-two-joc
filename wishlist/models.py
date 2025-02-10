a716656d91bf800c3b07273eba8eaa18b5f46c00
J O'Connor <jtoconnor75@gmail.com>
2025-02-10 17:42:14 +0000

    add styling changes to add-to-wishlist, planner, add-event

--- wishlist/models.py
+++ wishlist/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class WishlistItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item_name = models.CharField(max_length=255)
+    item_name = models.CharField(max_length=400)
     description = models.TextField(blank=True)
-    thumbnail_url = models.URLField(blank=True, null=True)
+    thumbnail_url = models.URLField(max_length=400, blank=True, null=True)
     link = models.URLField(blank=True)
 
     def __str__(self):
